bcf0d68a529cbef2ceaff26497f5f51fdddbbc8a
Your Name <your-email@example.com>
2024-09-12 02:23:39 +0300

    Initial commit

---
+++ gym.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    var toggleList = document.getElementById('toggleMenuList');
+    toggleList.classList.toggle('show');
+};
+
+function ballondor(){
+    var ballondors= document.querySelectorAll("imgone");
+   onclick="ballondor"
+};
+
+function redirectToWebsite(url) {
+    window.location.href = url;
+}
